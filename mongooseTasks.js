a634bdc7c4501b9e2c158a5bfdc60204565689f0
EldarTem <temirov.e.i.21@gmail.com>
2022-11-22 22:30:31 +0300

    Создание схемы

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,13 +1,16 @@
+
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/')
 
-var city = mongoose.model('city', { name: String })
+var schema = mongoose.Schema({ name: String })
 
-var samar = new city({ name: 'Самарканд' })
+schema.methods.meow = function(){
+    console.log(this.get("name") + ", salam")
+}
+
+var city = mongoose.model('city', schema)
+
+var samar = new city({ name: 'Samarkand' })
 samar.save(function (err) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('Hello Uzbekistan!')
-    }
+    samar.meow()
 })
