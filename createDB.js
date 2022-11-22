d54aa7a3c574a71bc2016ef1eab1f3f10232db81
EldarTem <temirov.e.i.21@gmail.com>
2022-11-22 21:58:59 +0300

    Посев данных приложения

--- createDB.js
+++ createDB.js
@@ -1,17 +1,16 @@
 var MongoClient = require('mongodb').MongoClient
 var data = require("./data.js").data
-
-const uri = "mongodb://localhost:27017/"
+const uri = "mongodb://127.0.0.1:27017/"
 const client = new MongoClient(uri)
 async function run() {
 try {
 await client.connect();
-var database = client.db("Samarkand");
+var database = client.db("samarkand");
 database.dropDatabase()
-database = client.db("Samarkandr");
-const cats = database.collection("Samarkand");
-const result = await cats.insertOne({name:"Самарканд"});
-console.log(`${result} documents were inserted`);
+database = client.db("samarkand");
+const sam = database.collection("sam");
+const result = await sam.insertMany(data);
+console.log(`${result.insertedCount} documents were inserted`);
 } finally {
 await client.close();
 }
