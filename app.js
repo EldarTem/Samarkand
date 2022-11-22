9c5d86fbf0b22daa30d805123d6f7de4c114f827
EldarTem <temirov.e.i.21@gmail.com>
2022-11-22 21:47:41 +0300

    Создание модуля данных

--- app.js
+++ app.js
@@ -36,7 +36,11 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', 
+  {
+    picture: "/public/images/error.png",
+    title: 'Интересно, произошла некая ошибка...'
+  });
 });
 
 module.exports = app;
