263370c3c02f6385739687befd75dce3e365ee42
EldarTem <temirov.e.i.21@gmail.com>
2022-11-30 22:19:18 +0300

    Извлечение данных из базы

--- routes/cit.js
+++ routes/cit.js
@@ -7,17 +7,15 @@ router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с city');
 });
 
-/* Страница героев */
+/* Страница городов*/
 router.get('/:nick', function(req, res, next) {
     City.findOne({nick:req.params.nick}, function(err, city){
           if(err) return next(err)
-          if(!city) return next(new Error("Нет такого места на планете"))
+          if(!city) return next(new Error("Нет такого города"))
           res.render('samarkand', {
               title: city.title,
               picture: city.avatar,
-              desc1: city.desc1,
-              desc2: city.desc2,
-              desc3: city.desc3
+              desc: city.desc,
           })
       })
   })
