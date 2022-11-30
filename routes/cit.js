256a4a76b5e4560abd88f101ceaa3486721a72e0
EldarTem <temirov.e.i.21@gmail.com>
2022-11-30 22:12:04 +0300

    Заполнение меню из базы данных

--- routes/cit.js
+++ routes/cit.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var City = require("../models/city").City
+var async = require("async")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,15 +10,26 @@ router.get('/', function(req, res, next) {
 
 /* Страница героев */
 router.get('/:nick', function(req, res, next) {
-    City.findOne({nick:req.params.nick}, function(err, city){
+  async.parallel([
+          function(callback){
+            City.findOne({nick:req.params.nick}, callback)
+          },
+          function(callback){
+            City.find({},{_id:0,title:1,nick:1},callback)
+          }
+      ],
+      function(err,result){
           if(err) return next(err)
+          var city = result[0]
+          var cit = result[1] || []
           if(!city) return next(new Error("Нет такого места на планете"))
           res.render('samarkand', {
               title: city.title,
               picture: city.avatar,
               desc: city.desc,
-          })
+              menu: cit
+          });
       })
-  })
+})
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var City = require("../models/city").City
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  City.find({}, { _id: 0, title: 1, nick: 1 }, function (err, menu) {
+    res.render('index', {
+      title: 'Express',
+      menu: menu
+    });
+  })
+
+});
 /*
 /* GET home page. 
 router.get('/', function(req, res, next) {
