debd0a239893924e5f6d407810b0978dc3e2d25b
EldarTem <temirov.e.i.21@gmail.com>
2022-12-06 22:44:49 +0300

    Сохранение session в mongodb

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,8 @@ var City = require("../models/city").City
 /* GET home page. */
 router.get('/', function (req, res, next) {
   City.find({}, { _id: 0, title: 1, nick: 1 }, function (err, menu) {
-    res.cookie('greeting', 'Hi!!!').render('index', { title: 'Express', menu:menu });
+    req.session.greeting = 'Hi!!!'
+    res.render('index', { title: 'Express', menu:menu });
   })
 
 });
