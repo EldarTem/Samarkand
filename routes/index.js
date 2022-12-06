9a077d137d30dbb1b193081debe9e0fb0d4304c4
EldarTem <temirov.e.i.21@gmail.com>
2022-12-06 22:34:58 +0300

    Команда записи в cookie

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,7 @@ var City = require("../models/city").City
 /* GET home page. */
 router.get('/', function (req, res, next) {
   City.find({}, { _id: 0, title: 1, nick: 1 }, function (err, menu) {
-    res.render('index', {
-      title: 'Express',
-      menu: menu
-    });
+    res.cookie('greeting', 'Hi!!!').render('index', { title: 'Express', menu:menu });
   })
 
 });
