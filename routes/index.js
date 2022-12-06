ebe91114e6db8764ec44f5b1c93e417418bfc335
EldarTem <temirov.e.i.21@gmail.com>
2022-12-06 22:51:09 +0300

    Создание счетчика посещения сайта

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,8 @@ var City = require("../models/city").City
 /* GET home page. */
 router.get('/', function (req, res, next) {
   City.find({}, { _id: 0, title: 1, nick: 1 }, function (err, menu) {
-    req.session.greeting = 'Hi!!!'
-    res.render('index', { title: 'Express', menu:menu });
+    res.render('index', { title: 'Express', menu: menu, counter: req.session.counter });
   })
-
 });
 /*
 /* GET home page. 
