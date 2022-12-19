02ed600c6e9279a96f97caa46dafed763ee0c76c
EldarTem <temirov.e.i.21@gmail.com>
2022-12-20 01:00:16 +0300

    Обаботка ошибки аутентификации

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/', function (req, res, next) {
 
 /* GET login/registration page. */
 router.get('/logreg', function(req, res, next) {
-  res.render('logreg',{title: 'Вход'});
+  res.render('logreg',{error:null});
   });
   
 /* POST login/registration page. */
@@ -25,7 +25,7 @@ router.post('/logreg', function (req, res, next) {
         req.session.user = user._id
         res.redirect('/')
       } else {
-        res.render('logreg', { title: 'Вход' })
+        res.render('logreg',{error:"Пароль не верный"});
       }
     } else {
       var user = new User({ username: username, password: password })
