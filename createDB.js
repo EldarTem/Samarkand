cb184f4de918b652980e1119556c531eea038b00
EldarTem <temirov.e.i.21@gmail.com>
2023-01-05 23:00:49 +0300

    Подключение MongoDB

--- createDB.js
+++ createDB.js
@@ -1,15 +1,15 @@
 var MongoClient = require('mongodb').MongoClient
 
-const uri = "mongodb://localhost:27017/"
+const uri = "mongodb://localhost:27017"
 const client = new MongoClient(uri)
 async function run() {
 try {
 await client.connect();
-var database = client.db("Samarkand");
+var database = client.db("city");
 database.dropDatabase()
-database = client.db("Samarkandr");
-const cats = database.collection("Samarkand");
-const result = await cats.insertOne({name:"Самарканд"});
+database = client.db("city");
+const sam = database.collection("sam");
+const result = await sam.insertOne({name:"Самарканд"});
 console.log(`${result} documents were inserted`);
 } finally {
 await client.close();
