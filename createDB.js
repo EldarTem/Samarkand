fdc6f9c8cdc0e4f8eb9f322df26979e12fc229d7
EldarTem <temirov.e.i.21@gmail.com>
2023-01-15 18:42:13 +0300

    Подключение MongoDB

--- createDB.js
+++ createDB.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient
 
-const uri = "mongodb://localhost:27017"
+const uri = "mongodb://127.0.0.1:27017/"
 const client = new MongoClient(uri)
 async function run() {
 try {
