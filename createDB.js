ca6dbff977fb77822f46a46417b146cf041e93b6
EldarTem <temirov.e.i.21@gmail.com>
2023-01-15 17:23:11 +0300

    Создание модуля данных

--- createDB.js
+++ createDB.js
@@ -1,16 +1,19 @@
 var MongoClient = require('mongodb').MongoClient
 var data = require("./data.js").data
-const uri = "mongodb://127.0.0.1:27017/"
+const uri = "mongodb://localhost:27017"
 const client = new MongoClient(uri)
 async function run() {
 try {
 await client.connect();
-var database = client.db("samarkand");
+var database = client.db("city");
 database.dropDatabase()
-database = client.db("samarkand");
+database = client.db("city");
 const sam = database.collection("sam");
 const result = await sam.insertMany(data);
 console.log(`${result.insertedCount} documents were inserted`);
+for (const key in result) {
+    console.log(`${key}: ${result[key]}`);
+    }
 } finally {
 await client.close();
 }
